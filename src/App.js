5235dc81836acb38778bb6f6dddee0807c5136ce
H4mxa <duke80800@gmail.com>
2021-07-01 00:21:13 +0500

    update menu-item to navigate withRouterHOC

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route } from "react-router-dom";
+import HomePage from "pages/homepage/HomePage";
+
+import "./App.css";
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Route exact path='/' component={HomePage} />
+      <Route exact path='/shop/hats' component={HatsPage} />
     </div>
   );
 }
