67c294b79449440d00b1124d3df7c89c3d27be6a
H4mxa <duke80800@gmail.com>
2021-07-21 08:42:56 +0500

    added cart-custom-buttom

--- src/components/collection-item/CollectionItem.jsx
+++ src/components/collection-item/CollectionItem.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import "./CollectionItem.scss";
 
+import CustomButton from 'components/custom-button/CustomButton'
+
 const CollectionItem = ({ name, price, imageUrl }) => {
   return (
     <div className='collection-item'>
       <div className='image' style={{ backgroundImage: `url(${imageUrl})` }} />
       <div className='collection-footer'>
         <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+        <span className='price'>${price}</span>
       </div>
+      <CustomButton inverted>Add To Cart</CustomButton>
     </div>
   );
 };
