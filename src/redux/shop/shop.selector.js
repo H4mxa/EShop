7a6256353d49cdc779c258477a0fecc329b61f1a
h4mxa <duke80800@gmail.com>
2021-08-28 03:45:57 +0500

    normalize shop data

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -1,12 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  mens: 4,
-};
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -15,8 +8,7 @@ export const selectCollections = createSelector(
 );
 
 export const selectCollection = (CollectionUrlParam) =>
-  createSelector([selectCollections], (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[CollectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    (collections) => collections[CollectionUrlParam]
   );
